a17cb5e8c540f7a6e6f9f6a097ac47f5fe242fd9
Akhila K S <128509685+akhilaks1702@users.noreply.github.com>
2025-02-14 15:12:05 +0530

    Find_Largest_Number_In_List.py

--- Find_Largest_Number_In_List.py
+++ Find_Largest_Number_In_List.py
@@ -1,6 +1,21 @@
-numbers = [2, 8, 13, 8888, 21]
-big = numbers[0]
-for number in numbers:
-    if number > big:
-        big = number
-print(f"Largest number in the list: {big}")
+class LargestNumber:
+
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+    def largest_number(self):
+        big = self.numbers[0]
+        for number in self.numbers:
+            if number > big:
+                big = number
+        print(f"Largest number in the list: {big}")
+
+numbers = [ 2, 65, 7, 9 ]
+number_list = LargestNumber(numbers)
+number_list.largest_number()
+
+
+
+
+
+
