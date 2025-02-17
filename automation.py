8daace8d1c8a69b279bc95e9afab14ff2450e0f3
Akhila K S <128509685+akhilaks1702@users.noreply.github.com>
2025-02-17 12:00:25 +0530

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,29 @@
+import openpyxl as xl
+from openpyxl.chart import Reference, BarChart
+
+def process_workbook(filename):
+
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+    corrected_price_cell_name = sheet['d1']
+    corrected_price_cell_name.value = 'corrected price'
+    chart_cell_name = sheet['f1']
+    chart_cell_name.value = 'chart'
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3) #this will give the price column
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+              min_row=2,
+              max_row=sheet.max_row,
+              min_col=4,
+              max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'f2')
+
+    wb.save(filename)
