2673f2f4d1201ca29578aa7ca452048c4895c560
Akhila K S <128509685+akhilaks1702@users.noreply.github.com>
2025-02-17 12:25:43 +0530

    Automation.py

--- automation.py
+++ automation.py
@@ -1,18 +1,16 @@
 import openpyxl as xl
 from openpyxl.chart import Reference, BarChart
 
-def process_workbook('transactions.xlsx'): 
+def process_workbook(filename):
 
-    wb = xl.load_workbook('transactions.xlsx')
+    wb = xl.load_workbook(filename)
     sheet = wb['Sheet1']
-    corrected_price_cell_name = sheet['d1']
-    corrected_price_cell_name.value = 'corrected price'
-    chart_cell_name = sheet['f1']
-    chart_cell_name.value = 'chart'
+    sheet['d1'] = 'corrected price'
+    sheet['f1'] = 'chart'
 
     for row in range(2, sheet.max_row + 1):
-        price = sheet.cell(row, 3) #this will give the price column
-        corrected_price = price.value * 0.9
+        cell = sheet.cell(row, 3) #this will give the price column
+        corrected_price = cell.value * 0.9
         corrected_price_cell = sheet.cell(row, 4)
         corrected_price_cell.value = corrected_price
 
@@ -26,4 +24,6 @@ def process_workbook('transactions.xlsx'):
     chart.add_data(values)
     sheet.add_chart(chart, 'f2')
 
-    wb.save(filename)
+    wb.save('transactions.xlsx')
+
+process_workbook('transactions.xlsx')
